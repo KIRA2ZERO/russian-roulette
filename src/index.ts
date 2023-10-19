44c5642e221dc9f874a92a70fa35b58e3d1499dc
idanran <96647698+idanran@users.noreply.github.com>
2023-10-19 13:49:10 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
+export const usage: string = `
+未给bot设置管理员权限将无法正常使用俄罗斯轮盘赌
+`
+
 declare module 'koishi' {
   interface Tables {
     russian_roulette_table: russian_roulette_table
@@ -38,29 +42,19 @@ export function apply(ctx: Context) {
     .usage('使用教程 https://github.com/KIRA2ZERO/russian-roulette')
     .example('俄罗斯轮盘赌 -n 9 -t 1800')
     .action(async({session,options}) => {
-
-     await session.onebot.getGroupMemberInfo(session.channelId,session.bot.selfId)
-     .then(async botInfo => {
-        if(botInfo['role'] != 'admin' && botInfo['role'] != 'owner'){
-          session.send(h('quote',{id:session.messageId})+`未给bot设置管理员权限无法启用俄罗斯轮盘赌`)
-        }else{
-          return await ctx.database.get('russian_roulette_table',{channel:session.channelId})
-        }
-     })
-      .then(async row => {
-        if(row.length !== 0){
-          session.send(h('quote',{id:session.messageId})+`每个群只能同时拥有一把左轮手枪`)
-        }else{
-          let number = options.number,
-              time = options.time,
-              clip = new Array(number).fill(''),
-              random = Math.floor(Math.random()*6);
-          for(let i = 0;i < clip.length; i++){i == random ? clip[i] = 'bullet':0}
-          ctx.database.create('russian_roulette_table',{clip:clip,channel:session.channelId,time:time})
-          session.send(h('quote',{id:session.messageId})+`载弹量为${number}发杀伤力为${time}秒的左轮手枪已装填,发送【扣下扳机】开枪`)
-        } 
-      })
-
+      const row = await ctx.database.get('russian_roulette_table',{channel:session.channelId})
+      
+      if(row.length !== 0){
+        session.send(h('quote',{id:session.messageId})+`每个群只能同时拥有一把左轮手枪`)
+      }else{
+        let number = options.number,
+          time = options.time,
+          clip = new Array(number).fill(''),
+          random = Math.floor(Math.random()*6);
+        for(let i = 0;i < clip.length; i++){i == random ? clip[i] = 'bullet':0}
+        ctx.database.create('russian_roulette_table',{clip:clip,channel:session.channelId,time:time})
+        session.send(h('quote',{id:session.messageId})+`载弹量为${number}发杀伤力为${time}秒的左轮手枪已装填,发送【扣下扳机】开枪`)
+      }
     })
 
   ctx.command('roulette.shoot','扣下扳机').alias(`扣下扳机`)
@@ -77,18 +71,8 @@ export function apply(ctx: Context) {
           time = row[0].time;
       if(clip[0] == 'bullet'){
         session.send(h('quote',{id:session.messageId})+'你死了');
-
-        let info =  await session.onebot.getGroupMemberInfo(session.channelId,session.userId);
-        const botInfo = await session.onebot.getGroupMemberInfo(session.channelId,session.bot.selfId)
-        if(botInfo['role'] === 'owner'){
-          session.onebot.setGroupBan(session.channelId,session.userId,time)
-        }else if(botInfo['role'] === 'admin'){
-          (info['role'] == 'member') ? session.onebot.setGroupBan(session.channelId,session.userId,time):session.send('权限不足禁言失败')
-        }else{
-          session.send('权限不足禁言失败')
-        }
-        ctx.database.remove('russian_roulette_table',{channel:session.channelId});
-
+        session.bot.muteGuildMember(session.guildId,session.userId,time)
+        ctx.database.remove('russian_roulette_table',{channel:session.channelId})
       }else{
         clip = clip.slice(1,clip.length)
         session.send(h('quote',{id:session.messageId})+`幸运存活但还剩${clip.length}颗子弹...`)
